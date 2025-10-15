13d0e8fdf9036ea9e469cf35b3326b841487154c
Dilshad P <dilshad.p@geekywolf.local>
2025-10-15 16:42:23 +0530

    added day 2 c sharp files

---
+++ c#_assignments/day2/day2/Q02.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace day2
+{
+    public class Q02
+    {
+        /// <summary>
+        /// Print odd numbers less than 50 using while loop
+        /// </summary>
+        public void Answer()
+        {
+            int counter = 0;
+            while (counter<50) 
+            {
+                if (counter % 2 != 0)
+                {
+                    Console.WriteLine("odd number = " + counter);
+                }
+                counter++;
+            }
+            Console.WriteLine();
+        }
+         
+    }
+    
+}
