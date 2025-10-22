195c5ba362ee35f6dff9fdc3c08f5182b5c30665
Dilshad P <dilshad.p@geekywolf.local>
2025-10-22 18:34:29 +0530

    added 3 csharp questions of day6 assignment

---
+++ c#_assignments/day6/day6/ConsoleApp.cs
@@ -0,0 +1,10 @@
+﻿using day6;
+
+ArraylistOperation q1 = new ArraylistOperation();
+//q1.Answer();
+
+ArraylistMixed q2 = new ArraylistMixed();
+//q2.Answer();
+
+Generics q3 = new Generics();
+q3.Answer();
